3033d8b363c34519781cca0b2864e59cfe2e1166
Nolan Perry Arnold <arnoldn@stolaf.edu>
2015-12-13 20:31:22 -0600

    Redid changes to optimize.cpp

--- Optimize/optimize.cpp
+++ Optimize/optimize.cpp
@@ -2,33 +2,12 @@
 #include<iostream>
 #include<string>
 #include"time.h"
+#include"optimize.h"
 #include"glareReduction.h" 
 #include"dataCollection.h"
 #include<cstdlib>
 Tile &loadJustOneTile(const string &tileID, const string &imgName);
 vector<PixelLoc> getPixelsFor(int);
-
-double calcNCC(vector<PixelLoc> *interior, double * current, Image *myimg, Image *myimgOther)
-{
-	Color black(0,0,0);
- 	Color white(255,255,255);
-	vector<Color> signal1,signal2;
-	double point[2];
-	for(unsigned int i=0; i<interior->size(); ++i){
-		homography(interior[0][i].x + 0.5 , interior[0][i].y + 0.5, current, point);
-                Coord mycoord(point[0], point[1]);
-                if(inImage(myimg,mycoord)){
-                    signal1.push_back(asInterpolatedColor(mycoord, myimg));
-                } else {
-                    signal1.push_back(black);
-                    signal2.push_back(white);
-                    continue;
-                }
-                signal2.push_back(myimgOther->getPixel(interior[0][i]));
-         }
-         return calculate_normalized_correlation(signal1, signal2);
-}
-
 
 int main(int argc, char **argv)
 {
@@ -45,9 +24,6 @@ int main(int argc, char **argv)
   double bestncc = -2;
   double first;
   double scale = 0.01;
-  bool initial = true;
-  int position = 0;
-  int direction = 1;
   bool optimize = true;
 
   vector<PixelLoc> interiorR = getContour(tile, imageR);
@@ -144,46 +120,12 @@ for(unsigned int i=0;i<fpDestination.size();++i){
    }
 }
 src.print("src.ppm");
-int count;
 cout << endl;
+
 if(optimize){
-	for(double i=1;i<=1000000;i*=10){
-	count = 0;
- 	cout <<"Scale = " << (scale/i) << endl;
-		
-	double offset = -50*(scale/i);
-	for(int l=0; l<2; ++l){
-	for(int k=0; k < 8; ++k){
-		for(int j=1; j<=100; ++j){
- 			ncc = calcNCC(&interior, current, &myimg, &myimgOther);
-			if (initial){
-				first = ncc;
-   				initial = false;
-			}
-			if (ncc > bestncc){
-				l=0;
-	   	       		++count;
-				bestncc = ncc;
-				best[k] = current[k];
-			}
-     		randHomography(k, init, current, offset + (scale/i)*j);
-    		}
-		cout << "Count: " << count << endl;
-		count = 0;
-		for(int j=0; j<9; ++j){
-			init[j] = current[j] =  best[j];
-		}	
-	}
-	}
-	
-	}
+        Optimize (scale, first, ncc, bestncc, &interior, init, current, best, &myimg, &myimgOther);
 }
-// cout << "Best so far: " << bestncc << endl;
-// cout << "homography: ";
-// for(int i=0;i<9;++i){
-//        cout << best[i] << " ";
-// }
-// cout << endl;
+
 
 Image imageWithGlare = myimg;
 
@@ -195,10 +137,7 @@ Image imageWithGlare = myimg;
 	cout << current[i] << " ";
  } 
  cout << endl;
-// cout << "Origonal Feature points: " ;
-// for(unsigned int i=0;i<fpDestination.size();++i){
-//	cout << fpDestination[i] << " ";
-// }
+
  cout << endl << "Optimized Feature points: ";
  for(unsigned int i=0;i<fpDestination.size();++i){
       homography(fpSource[i].x,fpSource[i].y, best, point);
