db09d245011c89f7441f851d9f1521f9ae85f774
magnusow <magnusow@stolaf.edu>
2015-12-13 20:25:57 -0600

    Minor dataCollection tweaks.

--- Optimize/optimize.cpp
+++ Optimize/optimize.cpp
@@ -2,12 +2,33 @@
 #include<iostream>
 #include<string>
 #include"time.h"
-#include"optimize.h"
 #include"glareReduction.h" 
 #include"dataCollection.h"
 #include<cstdlib>
 Tile &loadJustOneTile(const string &tileID, const string &imgName);
 vector<PixelLoc> getPixelsFor(int);
+
+double calcNCC(vector<PixelLoc> *interior, double * current, Image *myimg, Image *myimgOther)
+{
+	Color black(0,0,0);
+ 	Color white(255,255,255);
+	vector<Color> signal1,signal2;
+	double point[2];
+	for(unsigned int i=0; i<interior->size(); ++i){
+		homography(interior[0][i].x + 0.5 , interior[0][i].y + 0.5, current, point);
+                Coord mycoord(point[0], point[1]);
+                if(inImage(myimg,mycoord)){
+                    signal1.push_back(asInterpolatedColor(mycoord, myimg));
+                } else {
+                    signal1.push_back(black);
+                    signal2.push_back(white);
+                    continue;
+                }
+                signal2.push_back(myimgOther->getPixel(interior[0][i]));
+         }
+         return calculate_normalized_correlation(signal1, signal2);
+}
+
 
 int main(int argc, char **argv)
 {
@@ -15,6 +36,7 @@ int main(int argc, char **argv)
   string image = argv[2];
   string imageR = image+"R";
   string imageL = image+"L";
+  string smallerImage;
   double point[2];
   double * current = new double[9];
   double * best = new double[9];
@@ -23,6 +45,9 @@ int main(int argc, char **argv)
   double bestncc = -2;
   double first;
   double scale = 0.01;
+  bool initial = true;
+  int position = 0;
+  int direction = 1;
   bool optimize = true;
 
   vector<PixelLoc> interiorR = getContour(tile, imageR);
@@ -41,6 +66,7 @@ int main(int argc, char **argv)
     interior = getContour(tile, imageL);
     myH1  = getHomography(tile, imageL, imageR);
     myimg = imageR.c_str();
+    smallerImage = imageR;
     myimgOther = imageL.c_str();
     fpSource = getFeaturePoints(tile, imageL); 
     fpDestination = getFeaturePoints(tile, imageR); 
@@ -50,6 +76,7 @@ int main(int argc, char **argv)
     interior = getContour(tile, imageR);
     myH1  = getHomography(tile, imageR, imageL);
     myimg = imageL.c_str();
+    smallerImage = imageL;
     myimgOther = imageR.c_str();
     fpSource = getFeaturePoints(tile, imageR);
     fpDestination = getFeaturePoints(tile, imageL);
@@ -117,22 +144,61 @@ for(unsigned int i=0;i<fpDestination.size();++i){
    }
 }
 src.print("src.ppm");
+int count;
 cout << endl;
-
 if(optimize){
-        Optimize (scale, first, ncc, bestncc, &interior, init, current, best, &myimg, &myimgOther);
+	for(double i=1;i<=1000000;i*=10){
+	count = 0;
+ 	cout <<"Scale = " << (scale/i) << endl;
+		
+	double offset = -50*(scale/i);
+	for(int l=0; l<2; ++l){
+	for(int k=0; k < 8; ++k){
+		for(int j=1; j<=100; ++j){
+ 			ncc = calcNCC(&interior, current, &myimg, &myimgOther);
+			if (initial){
+				first = ncc;
+   				initial = false;
+			}
+			if (ncc > bestncc){
+				l=0;
+	   	       		++count;
+				bestncc = ncc;
+				best[k] = current[k];
+			}
+     		randHomography(k, init, current, offset + (scale/i)*j);
+    		}
+		cout << "Count: " << count << endl;
+		count = 0;
+		for(int j=0; j<9; ++j){
+			init[j] = current[j] =  best[j];
+		}	
+	}
+	}
+	
+	}
 }
+// cout << "Best so far: " << bestncc << endl;
+// cout << "homography: ";
+// for(int i=0;i<9;++i){
+//        cout << best[i] << " ";
+// }
+// cout << endl;
 
 Image imageWithGlare = myimg;
 
- int ncc_glare_reduced = calculateNCCWithoutGlare(&interior, best, &myimg, &myimgOther, &imageWithGlare);
+
+ double ncc_glare_reduced = calculateNCCWithoutGlare(&interior, best, &myimg, &myimgOther, &imageWithGlare);
  cout << "First: " << first << " Best: " << bestncc <<  "Glare Reduced: " << ncc_glare_reduced << endl;
  cout << "homography: "; 
  for(int i=0;i<9;++i){
 	cout << current[i] << " ";
  } 
  cout << endl;
- 
+// cout << "Origonal Feature points: " ;
+// for(unsigned int i=0;i<fpDestination.size();++i){
+//	cout << fpDestination[i] << " ";
+// }
  cout << endl << "Optimized Feature points: ";
  for(unsigned int i=0;i<fpDestination.size();++i){
       homography(fpSource[i].x,fpSource[i].y, best, point);
@@ -155,14 +221,10 @@ for(unsigned int i=0;i<fpDestination.size();++i){
       imgFinal.setPixel(loc,red);
    }
 }
-
-dataCollection entry(1, tile, myimg, imageR, imageL, first, best, ncc_glare_reduced, myH1.m, best);
-entry.writeEntry();
-
+dataCollection entry(1, tile, smallerImage, imageR, imageL, first, bestncc, ncc_glare_reduced, myH1.m, best);
 imagename = "TileImages/" + tile + "_final.ppm";
 imgFinal.print(imagename.c_str());
 imagename = "TileImages/" + tile + "_glare.ppm";
 imageWithGlare.print(imagename.c_str());
 system("/home/mscs/bin/show src.ppm initial.ppm final.ppm glare.ppm");
-
 }
